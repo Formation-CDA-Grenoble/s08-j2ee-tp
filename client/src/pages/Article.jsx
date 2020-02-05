910e179c258be3b6a0d3aa47f56d9d937b3799b7
Romain Tello <romain.tello@gmail.com>
2020-02-05 23:01:23 +0100

    Add claps

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -3,17 +3,20 @@ import { Layout } from '../components';
 import { articles } from '../data';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignLanguage } from '@fortawesome/free-solid-svg-icons';
 
 const Article = props => {
   const id = props.match.params.id;
   const article = articles[id];
-  const { title, author, content, date } = article;
+  const { title, author, content, date, claps } = article;
 
   return (
     <Layout>
       <h1>{title}</h1>
       <p>Posté par {author.name} le {date.toLocaleString()}</p>
       <p>{content}</p>
+      <Button variant="success"><FontAwesomeIcon icon={faSignLanguage} /> {claps} applaudissements</Button>
       <Link to='/'>
         <Button variant="secondary">Retour à l'accueil</Button>
       </Link>
